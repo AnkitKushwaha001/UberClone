fe7a77a8a248945cceff4aa42fc0dc418198db15
Ankit Kushwaha <ankitkumar963012@gmail.com>
2025-04-10 20:34:51 +0530

    register user

--- uber-express/app.js
+++ uber-express/app.js
@@ -2,17 +2,29 @@
 // npm i express
 // npm i dotenv cors
 // npm i nodemon -> start with = npx nodemon
+// npm i mongoose
+// npm i bcrypt jsonwebtoken
+// npm i express-validator
+// npm install --save-dev @types/express
 
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require("express");
 const cors = require('cors');
 const app = express();
+const connectToDb = require('./db/db')
+const userRoute = require('./routes/user.route')
+
+connectToDb();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.get("", (req, res) => {
+app.get("/", (req, res) => {
     res.send("Hello world");
 })
+
+app.use('/users', userRoute);
 
 module.exports = app;
---
+++ uber-express/models/user.model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        firstName: {
+            type: String,
+            require: true,
+            minlenght: [3, 'first name must be at least 3 character long'],
+        },
+        lastName: {
+            type: String,
+            minlenght: [3, 'last name must be at least 3 character long'],
+        }
+    },
+    email: {
+        type: String,
+        require: true,
+        minlength: [5, 'email must be at least 5 character long'],
+    },
+    password: {
+        type: String,
+        require: true,
+        select: false,
+    },
+    socketId: {
+        type: String
+    },
+})
+
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({_id: this._id}, process.env.JWT_SECRET)
+    return token;
+}
+
+userSchema.methods.comparePasswoerd = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.statics.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
+}
+
+const userModel = mongoose.model('user', userSchema);
+module.exports = userModel;
