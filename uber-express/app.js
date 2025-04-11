936774c6f42375034f1b4c5ce66ad6929d1fdf49
Ankit Kushwaha <ankitkumar963012@gmail.com>
2025-04-11 13:51:38 +0530

    login profile and logout routes  done

--- uber-express/app.js
+++ uber-express/app.js
@@ -6,12 +6,16 @@
 // npm i bcrypt jsonwebtoken
 // npm i express-validator
 // npm install --save-dev @types/express
+// npm i cookie-parser
+
+// Copilot -> GitHub Copilot Chat
 
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require("express");
 const cors = require('cors');
 const app = express();
+const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db')
 const userRoute = require('./routes/user.route')
 
@@ -20,6 +24,7 @@ connectToDb();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.send("Hello world");
--- uber-express/controllers/user.controller.js
+++ uber-express/controllers/user.controller.js
@@ -1,8 +1,10 @@
 const userModel = require('../models/user.model');
 const userService = require('../services/user.service')
 const { validationResult } = require('express-validator');
+const blacklistTokenModel = require('../models/blacklist.model');
 
-module.exports.registerUser = async(req, res, nest) => {
+// register-user
+module.exports.registerUser = async(req, res, next) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json({errors: errors.array()});
@@ -21,4 +23,40 @@ module.exports.registerUser = async(req, res, nest) => {
     const token = user.generateAuthToken();
 
     res.status(201).json({token, user})
+}
+
+// login-user
+module.exports.loginUser = async(req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {email, password} = req.body;
+
+    const user = await userModel.findOne({email}).select('+password');
+    if(!user) {
+        return res.status(401).json({message: 'Invalid email or password'});
+    }
+    
+    const isMatch = await user.comparePasswoerd(password);
+    if(!isMatch) {
+        return res.status(401).json({message: 'Invalid email or password'});
+    }
+
+    const token = user.generateAuthToken();
+
+    res.cookie('token', token);
+    res.status(200).json({token, user});
+}
+
+module.exports.getUserProfile = async(req, res, next) => {
+    res.status(200).json(req.user);
+}
+
+module.exports.logoutUser = async(req, res, next) => {
+    res.clearCookie('token');
+    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+    await blacklistTokenModel.create({token});
+    res.status(200).json({message: 'Logged out'});
 }
